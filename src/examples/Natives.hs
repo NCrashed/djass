f6a83de56abd5ec910b53e75242f46a474f2ec6d
ncrashed <ncrashed@gmail.com>
2015-02-12 04:03:41 +0400

    Random generation natives

--- src/examples/Natives.hs
+++ src/examples/Natives.hs
@@ -7,6 +7,7 @@ import Foreign.C.String
 import Foreign.Ptr
 import Control.Applicative
 import Control.Monad.IO.Class
+import System.Random  
 
 type NativeWriteln = CString -> IO ()
 nativeWriteln :: NativeWriteln
@@ -24,7 +25,29 @@ nativeI2S :: NativeI2S
 nativeI2S i = toJass.show =<< fromJass i
 foreign import ccall "wrapper" mkNativeI2S :: NativeI2S -> IO (FunPtr NativeI2S)
 
+type NativeGetRandInt = CInt -> CInt -> IO CInt
+nativeGetRandInt :: NativeGetRandInt
+nativeGetRandInt low high 
+  | low > high = return 0
+  | otherwise  = curry randomRIO low high  
+foreign import ccall "wrapper" mkNativeGetRandInt :: NativeGetRandInt -> IO (FunPtr NativeGetRandInt)
+
+type NativeGetRandReal = CFloat -> CFloat -> IO CFloat
+nativeGetRandReal :: NativeGetRandReal
+nativeGetRandReal low high 
+  | low > high = return 0
+  | otherwise  = curry randomRIO low high  
+foreign import ccall "wrapper" mkNativeGetRandReal :: NativeGetRandReal -> IO (FunPtr NativeGetRandReal)
+
+type NativeGetRandBool = IO CChar
+nativeGetRandBool :: NativeGetRandBool
+nativeGetRandBool = randomRIO (0, 1)
+foreign import ccall "wrapper" mkNativeGetRandBool :: NativeGetRandBool -> IO (FunPtr NativeGetRandBool)
+
 makeNativeTable :: NativeTableMaker
 makeNativeTable _ = liftIO $ sequence [ ("writeln",) . castFunPtr <$> mkNativeWriteln nativeWriteln
                                       , ("write",) . castFunPtr <$> mkNativeWrite nativeWrite
-                                      , ("I2S",) . castFunPtr <$> mkNativeI2S nativeI2S ]
+                                      , ("I2S",) . castFunPtr <$> mkNativeI2S nativeI2S
+                                      , ("getRandInt",) . castFunPtr <$> mkNativeGetRandInt nativeGetRandInt
+                                      , ("getRandReal",) . castFunPtr <$> mkNativeGetRandReal nativeGetRandReal
+                                      , ("getRandBool",) . castFunPtr <$> mkNativeGetRandBool nativeGetRandBool ]
--- tests/rosetta/console.j
+++ tests/rosetta/console.j
@@ -1,3 +1,7 @@
 native writeln takes string msg returns nothing
 native write takes string msg returns nothing
 native I2S takes integer i returns string
+
+native getRandInt takes integer low, integer high returns integer
+native getRandReal takes real low, real high returns real
+native getRandBool takes nothing returns boolean
